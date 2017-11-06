749b40847c6b019d7e3f1bffd42c4432fe229f01
Michael Pearce <michael@rootofpi.co.uk>
2017-11-06 08:57:44 +0000

    General updates * removes temp results file on completion * displays account number with profile names automatically * displays help text when no flag is provided

--- aws-info.sh
+++ aws-info.sh
@@ -2,14 +2,18 @@
 
 function findbypublicip {
   publicip=$1
-  resultfile=result_file_$profile.txt
+  resultfile=result_file.txt
   aws ec2 --profile $profile describe-instances --filters "Name=ip-address,Values=$publicip" --output table > $resultfile
   numberoflines=`wc -l $resultfile | sed 's/[^0-9]*//g'`
   if (( $numberoflines > 4  )); then
-    echo "Profile: $profile" && cat $resultfile
+    account=`getaccountid $profile`
+    echo "Profile: $profile" 
+    echo "Account ID: $account"
+    cat $resultfile
   else echo "Nothing found here."
   fi
 
+  rm $resultfile
 }
 
 function findbyinstancename {
@@ -18,18 +22,45 @@ function findbyinstancename {
   aws ec2 --profile $profile describe-instances --filters "Name=tag:Name,Values=$instancename" --output table > $resultfile
   numberoflines=`wc -l $resultfile | sed 's/[^0-9]*//g'`
   if (( $numberoflines > 4  )); then
-    echo "Profile: $profile" && cat $resultfile
+    account=`getaccountid $profile`
+    echo "Profile: $profile"
+    echo "Account ID: $account"
+    cat $resultfile
   else echo "Nothing found here."
   fi
+
+  rm $resultfile
+}
+
+
+function findbyinstanceid {
+  instanceid=$1
+  resultfile=result_file.txt
+  aws ec2 --profile $profile describe-instances --filters "Name=instance-id,Values=$instanceid" --output table > $resultfile
+  numberoflines=`wc -l $resultfile | sed 's/[^0-9]*//g'`
+  if (( $numberoflines > 4  )); then
+    account=`getaccountid $profile`
+    echo "Profile: $profile"
+    echo "Account ID: $account"
+    cat $resultfile
+  else echo "Nothing found here."
+  fi
+
+  rm $resultfile
 }
 
 function printprofiles {
   sudo cat ~/.aws/config | grep profile | cut -d " " -f 2 | sed 's/]//g'
 }
 
+function profilewithaccountid {
+  profile=$1
+  account=`getaccountid $profile`
+  echo "Profile: $profile; Account ID: $account"
+}
+
 function getaccountid {
   profile=$1
-  resultfile=result_file.txt
   aws --profile $profile sts get-caller-identity --output text --query 'Account'
 }
 
@@ -43,7 +74,7 @@ if [ $OPTIND -lt 1 ]; then
   exit 0;
 fi
 
-while getopts ":i:n:pa:" opt; do
+while getopts ":i:d:n:pa:" opt; do
   case $opt in
     i)
       selectedfunction="findbypublicip $OPTARG"
@@ -51,10 +82,13 @@ while getopts ":i:n:pa:" opt; do
     n)
       selectedfunction="findbyinstancename $OPTARG"
     ;;
+    d)
+      selectedfunction="findbyinstanceid $OPTARG"
+    ;;
     p)
-      echo "To print a list of AWS profiles that are configured, you will be asked for your sudo password"
-      printprofiles
-      exit 0;
+     selectedfunction="profilewithaccountid \$profile"
+     # printprofiles
+     # exit 0;
     ;;
     a)
       getaccountid $OPTARG
@@ -65,23 +99,23 @@ while getopts ":i:n:pa:" opt; do
     -i <public IP> find by public ip
     -n <instance name> find by instance name
     -p list configured aws profiles
-      -a <profile name> find account id for configured aws profile"
+    -a <profile name> find account id for configured aws profile"
       exit 0;
     ;;
   esac
 done
 
-# main function - loop through profiles to find what you are looking for
-
 if [ $OPTIND -eq 1 ]; then echo "No options were passed. Help - available options:
     -i <public IP> find by public ip
     -n <instance name> find by instance name
     -p list configured aws profiles
-      -a <profile name> find account id for configured aws profile"
+    -a <profile name> find account id for configured aws profile"
       exit 0;
 fi
 
+# main function - loop through profiles to find what you are looking for
 profile_list=`printprofiles`
 for profile in $profile_list; do
-  $selectedfunction
+  echo "Checking in the account profile: $profile"
+  eval $selectedfunction
 done
